a126904105ff623da140c64f84b5dbc37c6cfba4
dane.dung <dane.dung@itlvn.com>
2022-08-01 17:21:29 +0700

    restructer API

---
+++ dungAPI/Builder/DungBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace dungEcommerceAPI.Builder
+{
+    public static class DungBuilder
+    {
+        public static void UseDungBuilder(this IApplicationBuilder buider, bool isDevelopment)
+        {
+            // Configure the HTTP request pipeline.
+            if (isDevelopment)
+            {
+                buider.UseSwagger();
+                buider.UseSwaggerUI();
+            }
+
+            buider.UseHttpsRedirection();
+
+            buider.UseAuthorization();
+        }
+    }
+}
---
+++ dungAPI/ServiceRegister/DungServiceRegister.cs
@@ -0,0 +1,26 @@
+﻿using dungAPI.DAL.Context;
+using dungEcommerce.BLL.IRepositories;
+using dungEcommerce.BLL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace dungEcommerceAPI.ServiceRegister
+{
+    public static class DungServiceRegister
+    {
+        public static void AddDungServiceRegister(this IServiceCollection Services,IConfiguration configuration)
+        {
+            // Add services to the container.
+
+            Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            Services.AddEndpointsApiExplorer();
+            Services.AddSwaggerGen();
+
+            Services.AddDbContext<dungContext>(x => x.UseSqlServer(configuration.GetConnectionString("dungConnectionString")));
+
+            Services.AddTransient<ICategoryRepository, CategoryRepository>();
+
+            Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
